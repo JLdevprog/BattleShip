932097cf87a4213a6174d2ea425021e690920d1f
J.L <j.l.devprog@gmail.com>
2019-03-25 11:27:12 +0100

    Updt

---
+++ script.js
@@ -0,0 +1,13 @@
+var model = {
+	boardSize: 7;
+	numShips: 3;
+	shipLenght: 3;
+	shipSunk: 0;
+
+	ships: [
+		{ location:[0,0,0], hits['','','',]},
+		{ location:[0,0,0], hits['','','',]},
+		{ location:[0,0,0], hits['','','',]}
+	]
+}
+
